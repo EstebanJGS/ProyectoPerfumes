6a81abbad35bfad8daac7a26c178bd78f9226530
Less-707 <lhernandezl2403@alumno.ipn.mx>
2026-01-02 20:34:36 -0600

    CommitPrueba

---
+++ ProyectPerfumesz/src/main/java/com/mycompany/proyectperfumesz/ProyectPerfumesz.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectperfumesz;
+
+/**
+ *
+ * @author lesly
+ */
+public class ProyectPerfumesz {
+
+    public static void main(String[] args) {
+        System.out.println("Holas");
+    }
+}
